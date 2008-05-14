5593b3c94174dae5b904158ad2659047d188ce28
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2008-05-14 19:22:10 +0000

    Moved nlmer tests to the tests directory, added help page for refit, new release
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@130 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/nlmer.R
+++ pkg/tests/nlmer.R
@@ -1,6 +1,6 @@
 library(lme4)
 (nm2 <- nlmer(conc ~ SSfol(Dose, Time,lKe, lKa, lCl) ~ (lKe+lKa+lCl|Subject),
-              Theoph, start = c(lKe = -2.5, lKa = 0.5, lCl = -3)))
+              Theoph, start = c(lKe = -2.5, lKa = 0.5, lCl = -3), verb = TRUE))
 (nm3 <- nlmer(conc ~ SSfol(Dose, Time,lKe, lKa, lCl) ~
               (lKe|Subject) + (lKa|Subject) + (lCl|Subject), Theoph,
               start = c(lKe = -2.5, lKa = 0.5, lCl = -3), verbose = 1))
