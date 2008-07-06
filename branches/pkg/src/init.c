bd771a0ec68ce51dfc16c71982090133553c0d74
bdai <bdai@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-06 17:56:48 +0000

    Create branch
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@184 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/pkg/src/init.c
@@ -0,0 +1,101 @@
+#include "lmer.h"
+#include <R_ext/Rdynload.h>
+#include "Matrix.h"
+#include "Syms.h" 
+
+static R_CallMethodDef CallEntries[] = {
+/*     {"lme4_rWishart", (DL_FUNC) &lme4_rWishart, 3}, */
+
+    {"mer_MCMCsamp", (DL_FUNC) &mer_MCMCsamp, 2},
+    {"mer_ST_chol", (DL_FUNC) &mer_ST_chol, 1},
+    {"mer_ST_getPars", (DL_FUNC) &mer_ST_getPars, 1},
+    {"mer_ST_initialize", (DL_FUNC) &mer_ST_initialize, 3},
+    {"mer_ST_setPars", (DL_FUNC) &mer_ST_setPars, 2},
+    {"mer_create_L", (DL_FUNC) &mer_create_L, 1},
+    {"mer_optimize", (DL_FUNC) &mer_optimize, 2},
+    {"mer_postVar", (DL_FUNC) &mer_postVar, 1},
+    {"mer_update_L", (DL_FUNC) &mer_update_L, 1},
+    {"mer_update_RX", (DL_FUNC) &mer_update_RX, 1},
+    {"mer_update_dev", (DL_FUNC) &mer_update_dev, 1},
+    {"mer_update_projection", (DL_FUNC) &mer_update_projection, 1},
+    {"mer_update_ranef", (DL_FUNC) &mer_update_ranef, 1},
+    {"mer_update_mu", (DL_FUNC) &mer_update_mu, 1},
+    {"mer_update_u", (DL_FUNC) &mer_update_u, 2},
+    {"mer_validate", (DL_FUNC) &mer_validate, 1},
+
+    {"merMCMC_validate", (DL_FUNC) &merMCMC_validate, 1},
+    {"merMCMC_VarCorr", (DL_FUNC) &merMCMC_VarCorr, 2},
+
+    {"spR_optimize", (DL_FUNC) &spR_optimize, 2},
+    {"spR_update_mu", (DL_FUNC) &spR_update_mu, 1},
+/*     {"Zt_carryOver", (DL_FUNC) &Zt_carryOver, 4}, */
+
+    {NULL, NULL, 0}
+};
+
+/** cholmod_common struct local to the package */
+cholmod_common c;
+
+/** Initializer for lme4, called upon loading the package.
+ *
+ *  Register routines that can be called directly from R.
+ *  Initialize CHOLMOD and require the LL' form of the factorization.
+ *  Install the symbols to be used by functions in the package.
+ */
+#ifdef HAVE_VISIBILITY_ATTRIBUTE
+__attribute__ ((visibility ("default")))
+#endif
+void R_init_lme4(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+
+
+    M_R_cholmod_start(&c);
+    c.final_ll = 1;	    /* LL' form of simplicial factorization */
+
+    lme4_ASym = install("A");
+    lme4_CmSym = install("Cm");
+    lme4_CxSym = install("Cx");
+    lme4_DimSym = install("Dim");
+    lme4_GpSym = install("Gp");
+    lme4_LSym = install("L");
+    lme4_RXSym = install("RX");
+    lme4_RZXSym = install("RZX");
+    lme4_STSym = install("ST");
+    lme4_VSym = install("V");
+    lme4_XSym = install("X");
+    lme4_ZtSym = install("Zt");
+    lme4_devianceSym = install("deviance");
+    lme4_dimsSym = install("dims");
+    lme4_envSym = install("env");
+    lme4_etaSym = install("eta");
+    lme4_fixefSym = install("fixef");
+    lme4_flistSym = install("flist");
+    lme4_gradientSym = install("gradient");
+    lme4_iSym = install("i");
+    lme4_ncSym = install("nc");
+    lme4_nlmodelSym = install("nlmodel");
+    lme4_muEtaSym = install("muEta");
+    lme4_muSym = install("mu");
+    lme4_offsetSym = install("offset");
+    lme4_pSym = install("p");
+    lme4_permSym = install("perm");
+    lme4_pWtSym = install("pWt");
+    lme4_ranefSym = install("ranef");
+    lme4_residSym = install("resid");
+    lme4_sigmaSym = install("sigma");
+    lme4_sqrtrWtSym = install("sqrtrWt");
+    lme4_sqrtXWtSym = install("sqrtXWt");
+    lme4_uSym = install("u");
+    lme4_varSym = install("var");
+    lme4_xSym = install("x");
+    lme4_ySym = install("y");
+}
+
+/** Finalizer for lme4 called upon unloading the package.
+ *
+ */
+void R_unload_lme4(DllInfo *dll){
+    M_cholmod_finish(&c);
+}
---
+++ branches/www/index.php
@@ -0,0 +1,54 @@
+
+<!-- This is the project specific website template -->
+<!-- It can be changed as liked or replaced by other content -->
+
+<?php
+
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+$themeroot='http://r-forge.r-project.org/themes/rforge/';
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
+
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><?php echo $group_name; ?></title>
+	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+  </head>
+
+<body>
+
+<! --- R-Forge Logo --- >
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+<tr><td>
+<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+</table>
+
+
+<!-- get project title  -->
+<!-- own website starts here, the following may be changed as you like -->
+
+<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
+$contents = '';
+while (!feof($handle)) {
+	$contents .= fread($handle, 8192);
+}
+fclose($handle);
+echo $contents; } ?>
+
+<!-- end of project description -->
+
+<p> <strong>Doxygen</strong> documentation of the underlying C functions is <a href="./doxygen/"><strong>here</strong></a>. </p>
+
+
+<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+
+<p> <strong>References to articles and other research using nlme or lme4</strong> can be found <a href="bib/lme4bib.html"><strong>here</strong></a>. The LaTeX bibliography file can be accessed from <a href="bib/lme4bib.bib">here</a>.</p>
+
+</body>
+</html>
