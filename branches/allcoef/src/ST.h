d3173799e2dbcea6ece728f2834968a9f0b67135
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2009-02-08 17:39:06 +0000

    Enforce NO_C_HEADERS
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@279 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/allcoef/src/ST.h
+++ branches/allcoef/src/ST.h
@@ -1,6 +1,13 @@
 #ifndef LME4_ST_H
 #define LME4_ST_H
 
+#ifdef	__cplusplus
+#define NO_C_HEADERS
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#endif
+
 #include <R.h>
 /* Rdefines.h includes Rinternals.h (for SEXP, REAL, etc.) and defines
  * GET_SLOT, MAKE_CLASS, NEW_OBJECT, SET_SLOT, etc. */
--- branches/allcoef/src/lme4utils.cpp
+++ branches/allcoef/src/lme4utils.cpp
@@ -1,5 +1,3 @@
-#include <R.h>
-#include <Rdefines.h>
 #include "Matrix.h"
 
 #include "lme4utils.hpp"
