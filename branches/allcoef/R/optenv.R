9f1d61880b43c6d27575be34f8d9db0e6c52fc59
dmbates <dmbates@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-22 14:42:28 +0000

    ranef method for optimization environments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@287 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/allcoef/R/optenv.R
+++ branches/allcoef/R/optenv.R
@@ -36,3 +36,52 @@ setMethod("setPars", representation(x = "environment", pars = "numeric"),
                                                        pars[assign == i], x, ...))))
       })
 
+setMethod("ranef", signature(object = "environment"),
+          function(object, postVar = FALSE, drop = FALSE, whichel = names(ans), ...)
+      {
+          ## evaluate the list of matrices
+          ml <- ranef(object$rCF, u = object$u, perm = object$perm,
+                       postVar = postVar, ...)
+          ## produce a list of data frames corresponding to factors, not terms
+          fl <- object$flist
+          asgn <- attr(fl, "assign")
+          ans <- lapply(seq_along(fl),
+                        function(i)
+                            data.frame(do.call(cbind, ml[asgn == i]),
+                                       row.names = levels(fl[[i]]),
+                                       check.names = FALSE))
+          names(ans) <- names(fl)
+
+          ## Process whichel
+          stopifnot(is(whichel, "character"))
+          whchL <- names(ans) %in% whichel 
+          ans <- ans[whchL]
+
+          if (postVar) {
+### the desired calculation is a diagonal block of
+### sigma^2 Lambda(theta)P'L^{-T}L^{-1} P Lambda(theta)
+### rewrite this in a general form
+              pV <- .Call(ST_postVar, object$rCF, object$L,
+                          object$perm, object$flist, whchL)
+### need to multiply by sigma^2
+              dd <- object$dims
+              sc <- 1
+              if (dd["useSc"])
+                  sc <- object$deviance[if (dd["REML"]) "sigmaREML" else "sigmaML"]
+              sc <- sc * sc
+              for (i in seq_along(ans))
+                  attr(ans[[i]], "postVar") <- pV[[i]] * sc
+          }
+          if (drop)
+              ans <- lapply(ans, function(el)
+                        {
+                            if (ncol(el) > 1) return(el)
+                            pv <- drop(attr(el, "postVar"))
+                            el <- drop(as.matrix(el))
+                            if (!is.null(pv))
+                                attr(el, "postVar") <- pv
+                            el
+                        })
+          class(ans) <- "ranef.mer"
+          ans
+      })
