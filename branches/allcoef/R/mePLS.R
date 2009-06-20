ed664d9afd081fb7cec0c8e186d633a3ca39f778
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2009-06-20 16:50:28 +0000

    Change to Ut and V as arguments.  Check for changes in LDL status of factor.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@322 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/allcoef/R/mePLS.R
+++ branches/allcoef/R/mePLS.R
@@ -1,30 +1,32 @@
-##' Solve the penalized linear least squares problem associated with a
-##' mixed-effects model.
-
-##' @param Zt - transpose of sparse model matrix for the random effects
-##' @param Lambda - relative covariance factor
-##' @param y - response (or residual) vector
-##' @param X - optional fixed-effects model matrix
-##' @param L - optional sparse Cholesky factor
-##' @param super - should a supernodal decomposition be used.  Ignored
-##         if L is specified.
-
-##' @return a list with a newly created or update CHMfactor object L
-##' and the penalized least squares solution u.  If a fixed-effects
-##' model matrix X is given then the factors RZX and RX are also
-##' returned.
-
+##' Determine if a CHMfactor object is LDL or LL
+##' @param x - a CHMfactor object
+##' @return TRUE if x is LDL, otherwise FALSE
 isLDL <- function(x)
 {
     stopifnot(is(x, "CHMfactor"))
     as.logical(x@type[2])
 }
 
-mePLS <- function(Zt, Lambda, y, X, L, super = FALSE)
+##' Solve the penalized linear least squares problem associated with a
+##' mixed-effects model.
+
+##' @param Zt - crossprod(Lambda, Zt) where Zt is the transpose of
+##' sparse model matrix for the random effects and Lambda is the
+##' left relative covariance factor
+##' @param y - response (or residual) vector
+##' @param V - optional fixed-effects model matrix
+##' @param L - optional sparse Cholesky factor compatible with 
+##' @param super - logical scalar, should a supernodal decomposition
+##' be used? Ignored if L is specified.
+
+##' @return a list with a newly created or update CHMfactor object L
+##' and the penalized least squares solution u.  If a fixed-effects
+##' model matrix X is given then the factors RZX and RX are also
+##' returned.
+
+mePLS <- function(Ut, y, V, L, super = FALSE)
 {
-    stopifnot(is(Zt, "dgCMatrix"),
-              is(Lambda, "sparseMatrix"))
-    Ut <- crossprod(Lambda, Zt)
+    stopifnot(is(Ut, "dgCMatrix"))
     if (missing(L)) {
         L <- Cholesky(tcrossprod(Ut), LDL = FALSE, super = super,
                       Imult = 1)
@@ -33,7 +35,7 @@ mePLS <- function(Zt, Lambda, y, X, L, super = FALSE)
         L <- update(L, Ut, 1)
         stopifnot(isLDL(L))
     }
-    if (missing(X))
+    if (missing(V))
         return(list(L = L,
                     u = solve(L, Ut %*% as.numeric(y), system = "A")))
     PUtX <- solve(L, Ut %*% X, system = "P")
