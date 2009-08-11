5a300c15f5cd3ffe8229b278c27fe0afacc532ce
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-11 16:23:30 +0000

    consolidate all the small lmer() examples in lmerTest.R where some already were
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@408 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mlmRev/tests/Early.R
+++
@@ -1,5 +0,0 @@
-library(mlmRev)
-options(show.signif.stars = FALSE)
-Early$tos <- Early$age - 0.5
-(fm1 <- lmer(cog ~ tos * trt + (tos|id), Early))
-q("no")
--- pkg/mlmRev/tests/bdf.R
+++
@@ -1,6 +0,0 @@
-library(mlmRev)
-options(show.signif.stars = FALSE)
-(fm1 <- lmer(langPOST ~ IQ.ver.cen+avg.IQ.ver.cen+(IQ.ver.cen|schoolNR),
-             bdf))
-q("no")
-
