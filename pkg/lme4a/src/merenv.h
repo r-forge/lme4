94e039a64b6d25e4c28d739ba5c775791bef54f6
dmbates <dmbates@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-04 20:55:15 +0000

    Evaluate deviance using a C++ class
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@420 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/lme4a/src/merenv.h
@@ -0,0 +1,25 @@
+#ifndef LME4_MERENV_H
+#define LME4_MERENV_H
+
+#ifdef	__cplusplus
+#define NO_C_HEADERS
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#endif 
+
+#include <R.h>
+// Rdefines.h includes Rinternals.h (for SEXP, REAL, etc.) and defines
+// GET_SLOT, MAKE_CLASS, NEW_OBJECT, SET_SLOT, etc.
+#include <Rdefines.h>
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    SEXP lmerenv_deviance(SEXP rho, SEXP thnew);
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* LME4_MERENV_H */
