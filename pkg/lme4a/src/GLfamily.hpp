35ec859952b1a7840114293e6ae9a0c7771febbc
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2009-12-10 03:51:29 +0000

    Began new version of glmer
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@551 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/lme4a/src/GLfamily.hpp
@@ -0,0 +1,107 @@
+#ifndef LME4_GLFAMILY_HPP
+#define LME4_GLFAMILY_HPP
+
+class GLlink {
+public:
+    virtual void linkinv(double *mu, double *muEta,
+			 const double* eta, int n) = 0;
+    virtual const char* nm() = 0;
+    static const double LTHRESH;
+    static const double MLTHRESH;
+    static const double MPTHRESH;
+    static const double PTHRESH;
+    static const double INVEPS;
+};
+
+class logitlink : public GLlink {
+public:
+    void linkinv(double *mu, double *muEta, const double* eta, int n);
+    const char* nm() {return "logit";}
+};
+
+class probitlink : public GLlink {
+public:
+    void linkinv(double *mu, double *muEta, const double* eta, int n);
+    const char* nm() {return "probit";}
+};
+
+class clogloglink : public GLlink {
+public:
+    void linkinv(double *mu, double *muEta, const double* eta, int n);
+    const char* nm() {return "cloglog";}
+};
+
+class identitylink : public GLlink {
+public:
+    void linkinv(double *mu, double *muEta, const double* eta, int n);
+    const char* nm() {return "identity";}
+};
+
+class loglink : public GLlink {
+public:
+    void linkinv(double *mu, double *muEta, const double* eta, int n);
+    const char* nm() {return "log";}
+};
+
+class sqrtlink : public GLlink {
+public:
+    void linkinv(double *mu, double *muEta, const double* eta, int n);
+    const char* nm() {return "sqrt";}
+};
+
+class GLvar {
+public:
+    virtual void devResid(double *ans, const double *mu,
+			  const double *pw, const double *y,
+			  const int *fac, int n) = 0;
+    virtual void varFunc(double *var, const double *mu, int n) = 0;
+    virtual const char *distnm() = 0;
+};
+
+class constvar : public GLvar {
+public:
+    void devResid(double *ans, const double *mu,
+		  const double *pw, const double *y,
+		  const int *fac, int n);
+    void varFunc(double *var, const double *mu, int n);
+    const char *distnm() {return "gaussian";}
+};
+
+class mu1muvar : public GLvar {
+public:
+    void devResid(double *ans, const double *mu,
+		  const double *pw, const double *y,
+		  const int *fac, int n);
+    void varFunc(double *var, const double *mu, int n);
+    const char *distnm() {return "binomial";}
+};
+
+class muvar : public GLvar {
+public:
+    void devResid(double *ans, const double *mu,
+		  const double *pw, const double *y,
+		  const int *fac, int n);
+    void varFunc(double *var, const double *mu, int n);
+    const char *distnm() {return "poisson";}
+};
+
+class mu2var : public GLvar {
+public:
+    void devResid(double *ans, const double *mu,
+		  const double *pw, const double *y,
+		  const int *fac, int n);
+    void varFunc(double *var, const double *mu, int n);
+    const char *distnm() {return "Gamma";}
+};
+
+class mu3var : public GLvar {
+public:
+    void devResid(double *ans, const double *mu,
+		  const double *pw, const double *y,
+		  const int *fac, int n);
+    void varFunc(double *var, const double *mu, int n);
+    const char *distnm() {return "inverse.gaussian";}
+};
+
+#endif /* LME4_GLFAMILY_HPP */
+
