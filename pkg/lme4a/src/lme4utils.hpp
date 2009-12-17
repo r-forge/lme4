f573eee7e2013356a80dc6a6ceaad6e47de4a507
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2009-12-17 00:08:49 +0000

    merge classes by using virtual functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@574 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lme4a/src/lme4utils.hpp
+++ pkg/lme4a/src/lme4utils.hpp
@@ -12,6 +12,7 @@ double *VAR_REAL_NULL(SEXP rho, SEXP nm, int len, int nullOK,
 double *VAR_REAL_NULL(SEXP rho, SEXP nm, int len, int nullOK);
 double *VAR_REAL_NULL(SEXP rho, SEXP nm, int len);
 CHM_SP VAR_CHM_SP(SEXP rho, SEXP nm, int nrow, int ncol);
+CHM_FR VAR_CHM_FR(SEXP rho, SEXP nm, int n);
 double *VAR_dMatrix_x(SEXP rho, SEXP nm, int nrow, int ncol);
 
 CHM_SP CHM_SP_copy_in_place(CHM_SP dest, CHM_SP src);
