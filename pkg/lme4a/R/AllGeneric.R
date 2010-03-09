638d8bf9f0e9f4c90d16468e88768fdfcddad9f7
dmbates <dmbates@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-09 21:02:22 +0000

    Import fixef, ranef, VarCorr and BIC generics from nlme
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@697 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lme4a/R/AllGeneric.R
+++ pkg/lme4a/R/AllGeneric.R
@@ -3,28 +3,9 @@ setGeneric("lmList",
                     na.action, offset, pool, ...)
            standardGeneric("lmList"))
 
-if (FALSE) {
-setGeneric("gsummary",
-           function (object, FUN, form, level, groups,
-                     omitGroupingFactor = FALSE, 
-                     invariantsOnly = FALSE, ...)
-           standardGeneric("gsummary"))
-}
-
-setGeneric("evalDev", function(object, pars, ...) standardGeneric("evalDev"))
-
-setGeneric("fixef", function(object, ...) standardGeneric("fixef"))
-
-#' returns an A template from Zt
-setGeneric("create_A", function(rCF, rho, ...) standardGeneric("create_A"),
-           valueClass = "dgCMatrix")
-
 #' returns the parameter bounds
 setGeneric("getBounds", function(x, ...) standardGeneric("getBounds"),
            valueClass = "matrix")
-
-#' returns Lambda as a square sparse matrix
-setGeneric("getLambda", function(rCF, ...) standardGeneric("getLambda"))
 
 #' returns the current parameter values
 setGeneric("getPars", function(x, ...) standardGeneric("getPars"), valueClass = "numeric")
@@ -44,8 +25,6 @@ fixed.effects <- function(object, ...) {
     eval(mCall, parent.frame())
 }
 
-setGeneric("ranef", function(object, ...) standardGeneric("ranef"))
-
 random.effects <- function(object, ...) {
     ## random.effects was an alternative name for ranef
     .Deprecated("ranef")
@@ -53,13 +32,6 @@ random.effects <- function(object, ...) {
     mCall[[1]] = as.name("ranef")
     eval(mCall, parent.frame())
 }
-
-setGeneric("BIC", function(object, ...) standardGeneric("BIC"))
-
-setMethod("BIC", "logLik",
-          function(object, ...)
-          -2 * (c(object) - attr(object, "df") * log(attr(object, "nobs"))/2)
-          )
 
 if (FALSE) {
 setGeneric("HPDinterval",
@@ -72,13 +44,7 @@ setGeneric("mcmcsamp",
            standardGeneric("mcmcsamp"))
 }
 
-#setGeneric("pooledSD", function(x, ...) standardGeneric("pooledSD"))
-
 setGeneric("sigma", function(object, ...) standardGeneric("sigma"))
-
-setGeneric("VarCorr", function(x, ...) standardGeneric("VarCorr"))
-
-setGeneric("traceplot", function(x, ...) standardGeneric("traceplot"))
 
 if (FALSE) {
 setGeneric("refit", function(object, newresp, ...) standardGeneric("refit"))
