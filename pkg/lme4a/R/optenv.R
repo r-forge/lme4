1c5f2e03fa6cc1e958595e8c265161c1f31ff58d
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-15 17:06:53 +0000

    a version that also works in R <= 2.10.x   (so R-forge hopefully *builds* the package again!)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@706 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lme4a/R/optenv.R
+++ pkg/lme4a/R/optenv.R
@@ -10,6 +10,9 @@ setMethod("setPars", representation(x = "optenv", pars = "numeric"), function(x,
 
 setMethod("env", "optenv", function(x, ...) environment(x@getPars))
 
+## In newer versions of R,  the method argument list is *extended*
+## to the one of the generic
+if(getRversion() >= "2.11.0") {
 setMethod("nlminb", signature(start = "optenv"),
           function(start, ...)
       {
@@ -37,3 +40,37 @@ setMethod("optimize", signature(f = "optenv"),
           bb <- f@getBounds()
           optimize(f@setPars, lower = bb[,"lower"], upper = bb[,"upper"])
       })
+
+} else {## R <= 2.10.x -- need correct (full) argument list in method
+
+setMethod("nlminb", signature(start = "optenv"),
+          function (start, objective, gradient = NULL, hessian = NULL, ...,
+                    scale = 1, control = list(), lower = -Inf, upper = Inf)
+      {
+          ## the argument name 'control' is matched from the generic
+          control <- as.list(control)
+          bb <- start@getBounds()
+          nlminb(start@getPars(), start@setPars, lower = bb[,"lower"],
+                 upper = bb[, "upper"], control = control)
+      })
+
+setMethod("bobyqa", signature(par = "optenv"),
+          function(par, fn, lower = -Inf, upper = Inf, control = bobyqa.control(), ...)
+      {
+          ## the argument name 'control' is matched from the generic
+          control <- as.list(control)
+          bb <- par@getBounds()
+          bobyqa(par@getPars(), par@setPars, lower = bb[,"lower"],
+                 upper = bb[, "upper"], control = control)
+      })
+
+setMethod("optimize", signature(f = "optenv"),
+          function (f, interval, ..., lower = min(interval), upper = max(interval),
+                    maximum = FALSE, tol = .Machine$double.eps^0.25)
+      {
+          stopifnot(length(f@getPars()) == 1)
+          bb <- f@getBounds()
+          optimize(f@setPars, lower = bb[,"lower"], upper = bb[,"upper"])
+      })
+
+}## if( R >= 2.11.0 ) .... else ....
