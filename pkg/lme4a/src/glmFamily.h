d11e0b3aa5dcfb776dcd5ad5cc9dc7241f528797
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2010-04-08 12:44:18 +0000

    Add logit and probit links
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@728 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lme4a/src/glmFamily.h
+++ pkg/lme4a/src/glmFamily.h
@@ -3,6 +3,7 @@
 #define LME4_GLMFAMILY_H
 
 #include <Rcpp.h>
+#include <Rmath.h>
 
 typedef std::map<std::string, double(*)(double)> fmap;
 
@@ -31,14 +32,37 @@ private:
 	muEtas,			//< scalar muEta functions
 	varFuncs;		//< scalar variance functions
     Rcpp::List lst;
-    static double epsilon;
+    
+    static double epsilon, INVEPS, LTHRESH, MLTHRESH;
+
     static double sqrf(double x) {return x * x;}
     static double twoxf(double x) {return 2 * x;}
     static double identf(double x) {return x;}
     static double onef(double x) {return 1;}
     static double inversef(double x) {return 1/x;}
     static double invderivf(double x) {return -1/(x * x);}
-    
+
+    static double safeExp(double x) {
+	return (x < MLTHRESH) ? epsilon : ((x > LTHRESH) ? INVEPS : exp(x));
+    }
+    static double logitLinkInv(double x) {
+	double tmp = safeExp(x);
+	return tmp/(1 + tmp);
+    }
+    static double logitLink(double x) {return log(x/(1 - x));}
+    static double logitMuEta(double x) {
+	double tmp = safeExp(x);
+	double mu = tmp / (1 + tmp);
+	return mu * (1 - mu);
+    }
+
+    static double probitLink(double x) {return Rf_qnorm5(x, 0, 1, 1, 0);}
+    static double probitLinkInv(double x) {
+	return std::max(std::min(Rf_pnorm5(x, 0, 1, 1, 0), 1. - epsilon), epsilon);
+    }
+    static double probitMuEta(double x) {
+	return std::max(Rf_dnorm4(x, 0, 1, 0), epsilon);
+    }
 };
 
 #endif /* LME4_GLMFAMILY_H */
