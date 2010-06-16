a996f2835d4296a0d23fecec32ed9fee0986ae39
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2010-06-16 22:25:47 +0000

    Use a C++ namespace; add a test harness
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@920 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lme4a/src/glmFamily.cpp
+++ pkg/lme4a/src/glmFamily.cpp
@@ -5,102 +5,125 @@
 using namespace Rcpp;
 using namespace std;
 
-double glmFamily::epsilon = numeric_limits<double>::epsilon();
-double glmFamily::INVEPS = 1. / glmFamily::epsilon;
-double glmFamily::LTHRESH = 30.;
-double glmFamily::MLTHRESH = -30.;
+namespace glm {
+    double glmFamily::epsilon = numeric_limits<double>::epsilon();
+    double glmFamily::INVEPS = 1. / glmFamily::epsilon;
+    double glmFamily::LTHRESH = 30.;
+    double glmFamily::MLTHRESH = -30.;
+    
+    fmap glmFamily::linvs = fmap();
+    fmap glmFamily::lnks = fmap();
+    fmap glmFamily::muEtas = fmap();
+    fmap glmFamily::varFuncs = fmap();
+    
+    glmFamily::glmFamily(SEXP ll) : lst(ll) {
+	if (as<string>(lst.attr("class")) != "family")
+	    Rf_error("glmFamily only from list of (S3) class \"family\"");
+	
+	CharacterVector fam = lst["family"], llink = lst["link"];
+	char *pt = fam[0]; family = string(pt);
+	pt = llink[0]; link = string(pt);
+	
+	if (!lnks.count("identity")) { // initialize the static maps
+	    lnks["log"] = &log;
+	    muEtas["log"] = linvs["log"] = &exp;
+	    
+	    lnks["sqrt"] = &sqrt;
+	    linvs["sqrt"] = &sqrf;
+	    muEtas["sqrt"] = &twoxf;
+	    
+	    lnks["identity"] = linvs["identity"] = &identf;
+	    muEtas["identity"] = &onef;
+	    
+	    lnks["inverse"] = linvs["inverse"] = &inversef;
+	    muEtas["inverse"] = &invderivf;
+	    
+	    lnks["logit"] = &logitLink;
+	    linvs["logit"] = &logitLinkInv;
+	    muEtas["logit"] = &logitMuEta;
+	    
+	    lnks["probit"] = &probitLink;
+	    linvs["probit"] = &probitLinkInv;
+	    muEtas["probit"] = &probitMuEta;
+	    
+	    varFuncs["Gamma"] = &sqrf;             // x^2
+	    varFuncs["binomial"] = &x1mxf;         // x * (1 - x)
+	    varFuncs["inverse.gaussian"] = &cubef; // x^3
+	    varFuncs["gaussian"] = &onef;	   // 1
+	    varFuncs["poisson"] = &identf;	   // x
+	}
+    }
 
-fmap glmFamily::linvs = fmap();
-fmap glmFamily::lnks = fmap();
-fmap glmFamily::muEtas = fmap();
-fmap glmFamily::varFuncs = fmap();
+    void
+    glmFamily::linkFun(Rcpp::NumericVector &eta, Rcpp::NumericVector const &mu) {
+	if (lnks.count(link)) {
+	    transform(mu.begin(), mu.end(), eta.begin(), lnks[link]);
+	} else {
+	    Function linkfun = lst["linkfun"];
+	    NumericVector ans = linkfun(mu);
+	    copy(ans.begin(), ans.end(), eta.begin());
+	}
+    }
 
-glmFamily::glmFamily(SEXP ll) : lst(ll) {
-    if (as<string>(lst.attr("class")) != "family")
-	Rf_error("glmFamily only from list of (S3) class \"family\"");
-
-    CharacterVector fam = lst["family"], llink = lst["link"];
-    char *pt = fam[0]; family = string(pt);
-    pt = llink[0]; link = string(pt);
-
-    if (!lnks.count("identity")) { // initialize the static maps
-	lnks["log"] = &log;
-	muEtas["log"] = linvs["log"] = &exp;
-
-	lnks["sqrt"] = &sqrt;
-	linvs["sqrt"] = &sqrf;
-	muEtas["sqrt"] = &twoxf;
-
-	lnks["identity"] = linvs["identity"] = &identf;
-	muEtas["identity"] = &onef;
-
-	lnks["inverse"] = linvs["inverse"] = &inversef;
-	muEtas["inverse"] = &invderivf;
-
-	lnks["logit"] = &logitLink;
-	linvs["logit"] = &logitLinkInv;
-	muEtas["logit"] = &logitMuEta;
-
-	lnks["probit"] = &probitLink;
-	linvs["probit"] = &probitLinkInv;
-	muEtas["probit"] = &probitMuEta;
-
-	varFuncs["Gamma"] = &sqrf;             // x^2
-	varFuncs["binomial"] = &x1mxf;         // x * (1 - x)
-	varFuncs["inverse.gaussian"] = &cubef; // x^3
-	varFuncs["gaussian"] = &onef;	       // 1
-	varFuncs["poisson"] = &identf;	       // x
+    void
+    glmFamily::linkInv(Rcpp::NumericVector &mu, Rcpp::NumericVector const &eta) {
+	if (linvs.count(link)) {
+	    transform(eta.begin(), eta.end(), mu.begin(), linvs[link]);
+	} else {
+	    Function linkinv = lst["linkinv"];
+	    NumericVector ans = linkinv(eta);
+	    copy(ans.begin(), ans.end(), mu.begin());
+	}
+    }
+    
+    Rcpp::NumericVector
+    glmFamily::muEta(Rcpp::NumericVector const &eta) const {
+	if (muEtas.count(link)) {
+	    NumericVector ans(eta.size());
+	    transform(eta.begin(), eta.end(), ans.begin(), muEtas[link]);
+	    return ans;
+	}
+	Function mu_eta = ((const_cast<glmFamily*>(this))->lst)["mu.eta"];
+	return mu_eta(eta);
+    }
+    
+    Rcpp::NumericVector
+    glmFamily::variance(Rcpp::NumericVector const &mu) const {
+	if (varFuncs.count(link)) {
+	    NumericVector ans(mu.size());
+	    transform(mu.begin(), mu.end(), ans.begin(), varFuncs[link]);
+	    return ans;
+	}
+	Function vv = ((const_cast<glmFamily*>(this))->lst)["variance"];
+	return vv(mu);
+    }
+    
+    Rcpp::NumericVector
+    glmFamily::devResid(Rcpp::NumericVector const &mu,
+			Rcpp::NumericVector const &weights,
+			Rcpp::NumericVector const &y) const {
+	Function devres = ((const_cast<glmFamily*>(this))->lst)["dev.resids"];
+	return devres(y, mu, weights);
     }
 }
 
-void
-glmFamily::linkFun(Rcpp::NumericVector &eta, Rcpp::NumericVector const &mu) {
-    if (lnks.count(link)) {
-	transform(mu.begin(), mu.end(), eta.begin(), lnks[link]);
-    } else {
-	Function linkfun = lst["linkfun"];
-	NumericVector ans = linkfun(mu);
-	copy(ans.begin(), ans.end(), eta.begin());
-    }
-}
-
-void
-glmFamily::linkInv(Rcpp::NumericVector &mu, Rcpp::NumericVector const &eta) {
-    if (linvs.count(link)) {
-	transform(eta.begin(), eta.end(), mu.begin(), linvs[link]);
-    } else {
-	Function linkinv = lst["linkinv"];
-	NumericVector ans = linkinv(eta);
-	copy(ans.begin(), ans.end(), mu.begin());
-    }
-}
-
-Rcpp::NumericVector
-glmFamily::muEta(Rcpp::NumericVector const &eta) const {
-    if (muEtas.count(link)) {
-	NumericVector ans(eta.size());
-	transform(eta.begin(), eta.end(), ans.begin(), muEtas[link]);
+RCPP_FUNCTION_3(Rcpp::NumericVector,testFam,Rcpp::List fp,Rcpp::NumericVector x,std::string which) {
+    glm::glmFamily fam(fp);
+    if (which == "linkinv") {
+	Rcpp::NumericVector ans(x.size());
+	fam.linkInv(ans, x);
 	return ans;
     }
-    Function mu_eta = ((const_cast<glmFamily*>(this))->lst)["mu.eta"];
-    return mu_eta(eta);
-}
-
-Rcpp::NumericVector
-glmFamily::variance(Rcpp::NumericVector const &mu) const {
-    if (varFuncs.count(link)) {
-	NumericVector ans(mu.size());
-	transform(mu.begin(), mu.end(), ans.begin(), varFuncs[link]);
+    if (which == "linkfun") {
+	Rcpp::NumericVector ans(x.size());
+	fam.linkFun(ans, x);
 	return ans;
     }
-    Function vv = ((const_cast<glmFamily*>(this))->lst)["variance"];
-    return vv(mu);
-}
-
-Rcpp::NumericVector
-glmFamily::devResid(Rcpp::NumericVector const &mu,
-		    Rcpp::NumericVector const &weights,
-		    Rcpp::NumericVector const &y) const {
-    Function devres = ((const_cast<glmFamily*>(this))->lst)["dev.resids"];
-    return devres(y, mu, weights);
+    if (which == "mu.eta") {
+	return fam.muEta(x);
+    }
+    if (which == "variance") {
+	return fam.variance(x);
+    }
+    throw runtime_error("Unknown which value in testFam");
 }
--- pkg/lme4a/src/glmFamily.h
+++ pkg/lme4a/src/glmFamily.h
@@ -5,71 +5,72 @@
 #include <Rcpp.h>
 #include <Rmath.h>
 
-typedef std::map<std::string, double(*)(double)> fmap;
+namespace glm {
+    typedef std::map<std::string, double(*)(double)> fmap;
 
-
-class glmFamily {
-    std::string family, link;
-    Rcpp::List           lst;
-public:
-    glmFamily(SEXP);
-
-    void  linkFun(Rcpp::NumericVector&, Rcpp::NumericVector const&);
-    void  linkInv(Rcpp::NumericVector&, Rcpp::NumericVector const&);
-    Rcpp::NumericVector devResid(
-	Rcpp::NumericVector const&,
-	Rcpp::NumericVector const&,
-	Rcpp::NumericVector const&) const;
-    Rcpp::NumericVector    muEta(Rcpp::NumericVector const&) const;
-    Rcpp::NumericVector variance(Rcpp::NumericVector const&) const;
-private:
-    static fmap
-	devs,			//< scalar deviance functions
-	lnks,			//< scalar link functions
-	linvs,			//< scalar linkinv functions
-	muEtas,			//< scalar muEta functions
-	varFuncs;		//< scalar variance functions
+    class glmFamily {
+	std::string family, link;
+	Rcpp::List           lst;
+    public:
+	glmFamily(SEXP);
+	
+	void  linkFun(Rcpp::NumericVector&, Rcpp::NumericVector const&);
+	void  linkInv(Rcpp::NumericVector&, Rcpp::NumericVector const&);
+	Rcpp::NumericVector devResid(
+	    Rcpp::NumericVector const&,
+	    Rcpp::NumericVector const&,
+	    Rcpp::NumericVector const&) const;
+	Rcpp::NumericVector    muEta(Rcpp::NumericVector const&) const;
+	Rcpp::NumericVector variance(Rcpp::NumericVector const&) const;
+    private:
+	static fmap
+ 	    devs,			//< scalar deviance functions
+	    lnks,			//< scalar link functions
+	    linvs,			//< scalar linkinv functions
+	    muEtas,			//< scalar muEta functions
+	    varFuncs;		//< scalar variance functions
     
-    static double epsilon, INVEPS, LTHRESH, MLTHRESH;
+	static double epsilon, INVEPS, LTHRESH, MLTHRESH;
 
-    static double cubef(double x) {return x * x * x;}
-    static double identf(double x) {return x;}
-    static double invderivf(double x) {return -1/(x * x);}
-    static double inversef(double x) {return 1/x;}
-    static double onef(double x) {return 1;}
-    static double sqrf(double x) {return x * x;}
-    static double twoxf(double x) {return 2 * x;}
-    static double x1mxf(double x) {return std::max(epsilon, x * (1 - x));}
+	static double cubef(double x) {return x * x * x;}
+	static double identf(double x) {return x;}
+	static double invderivf(double x) {return -1/(x * x);}
+	static double inversef(double x) {return 1/x;}
+	static double onef(double x) {return 1;}
+	static double sqrf(double x) {return x * x;}
+	static double twoxf(double x) {return 2 * x;}
+	static double x1mxf(double x) {return std::max(epsilon, x * (1 - x));}
+	
+	static double finitePos(double x) { // truncate to [eps, 1/eps]
+	    return std::max(epsilon, std::min(INVEPS, x));
+	}
+	static double finite01(double x) { // truncate to [eps, 1 - eps]
+	    return std::max(epsilon, std::min(1. - epsilon, x));
+	}
+	
+	static double logitLinkInv(double x) {
+	    double tmp = finitePos(exp(x));
+	    return tmp/(1 + tmp);
+	}
+	static double logitLink(double x) {
+	    double xx = finite01(x);
+	    return log(xx / (1 - xx));
+	}
+	static double logitMuEta(double x) {
+	    return x1mxf(logitLinkInv(x));
+	}
+	
+	static double probitLink(double x) {
+	    return Rf_qnorm5(x, 0, 1, 1, 0);
+	}
+	static double probitLinkInv(double x) {
+	    return finite01(Rf_pnorm5(x, 0, 1, 1, 0));
+	}
+	static double probitMuEta(double x) {
+	    return finitePos(Rf_dnorm4(x, 0, 1, 0));
+	}
+    };
+}
     
-    static double finitePos(double x) { // truncate to [eps, 1/eps]
-	return std::max(epsilon, std::min(INVEPS, x));
-    }
-    static double finite01(double x) { // truncate to [eps, 1 - eps]
-	return std::max(epsilon, std::min(1. - epsilon, x));
-    }
-
-    static double logitLinkInv(double x) {
-	double tmp = finitePos(exp(x));
-	return tmp/(1 + tmp);
-    }
-    static double logitLink(double x) {
-	double xx = finite01(x);
-	return log(xx / (1 - xx));
-    }
-    static double logitMuEta(double x) {
-	return x1mxf(logitLinkInv(x));
-    }
-
-    static double probitLink(double x) {
-	return Rf_qnorm5(x, 0, 1, 1, 0);
-    }
-    static double probitLinkInv(double x) {
-	return finite01(Rf_pnorm5(x, 0, 1, 1, 0));
-    }
-    static double probitMuEta(double x) {
-	return finitePos(Rf_dnorm4(x, 0, 1, 0));
-    }
-};
-
 #endif /* LME4_GLMFAMILY_H */
 
