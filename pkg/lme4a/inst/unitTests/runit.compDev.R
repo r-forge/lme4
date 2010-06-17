a6a3f0d9dbfe7688f5fc4dd7f3e368566b03d1ca
dmbates <dmbates@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-17 18:30:56 +0000

    Added tests comparing compiled deviance function and R/Matrix evaluation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@925 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/lme4a/inst/unitTests/runit.compDev.R
@@ -0,0 +1,5 @@
+fm1 <- lmer(Yield ~ 1|Batch, Dyestuff, doFit = FALSE)
+dd1c <- mkdevfun(fm1)
+dd1u <- mkdevfun(fm1, compDev = FALSE)
+checkEquals(dd1c(1), dd1u(1))
+checkEquals(bobyqa(1,dd1c,0), bobyqa(1,dd1u,0))
