84b3b0daa520ca3135e866feaa1c3cf12e2ecffe
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2010-07-07 14:40:50 +0000

    Contents moved to Matrix/R/spModels.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@991 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lme4a/R/glm.R
+++
@@ -1,9 +0,0 @@
-setAs("ddenseModelMatrix", "predModule",
-      function(from)
-      new("dPredModule", coef = numeric(ncol(from)),
-          X = from, fac = chol(crossprod(from))))
-
-setAs("dsparseModelMatrix", "predModule",
-      function(from)
-      new("sPredModule", coef = numeric(ncol(from)),
-          X = from, fac = Cholesky(crossprod(from))))
