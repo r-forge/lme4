a6dd22a54f0eb5de652729b153955189ecfe4306
dmbates <dmbates@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-08 21:56:34 +0000

    Remove cached value of sqrLenU and calculate it on the fly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@1132 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lme4a/src/reModule.cpp
+++ pkg/lme4a/src/reModule.cpp
@@ -86,19 +86,8 @@ namespace mer{
 	    throw std::runtime_error("size mismatch");
 	    // Rf_error("%s: expected %s.size() = %d, got %d",
 	    // 	     "reModule::setU", "ubase", q, ubase.size());
-//	NumericVector res = (step == 0.) ? ubase : ubase + incr * step; // Needs Rcpp_0.8.3
-//	copy(res.begin(), res.end(), d_u.begin());
-	if (step == 0.) {
-	    copy(ubase.begin(), ubase.end(), d_u.begin());
-	} else {
-	    if (incr.size() != q)
-	 	throw std::runtime_error("reModule::setU size mismatch of u and incr");
-	    transform(incr.begin(), incr.end(), d_u.begin(),
-	 	      bind2nd(multiplies<double>(), step));
-	    transform(ubase.begin(), ubase.end(),
-	 	      d_u.begin(), d_u.begin(), plus<double>());
-	}
-	d_sqrLenU = inner_product(d_u.begin(), d_u.end(), d_u.begin(), double());
+	NumericVector res = (step == 0.) ? ubase : ubase + incr * step;
+	copy(res.begin(), res.end(), d_u.begin());
     }
 
     /** 
@@ -119,18 +108,13 @@ namespace mer{
      * @param nt New value of theta
      */
     void reModule::updateLambda(const Rcpp::NumericVector& nt) {
-//	if (any(nt < d_lower).is_true()) // Needs Rcpp_0.8.3
-//	    throw runtime_error("updateLambda: theta not in feasible region");
-				// check that nt is feasible
 	R_len_t nth = d_lower.size();
 	if (nt.size() != nth)
 	    throw runtime_error("updateLambda: size mismatch of nt and d_lower");
-	double *ll = d_lower.begin(), *th = nt.begin();
-	for (R_len_t i = 0; i < nth; ++i)
-	    if (th[i] < ll[i])
-	 	throw runtime_error("updateLambda: theta not in feasible region");
+	if (any(nt < d_lower).is_true()) // check that nt is feasible
+	    throw runtime_error("updateLambda: theta not in feasible region");
 				// update Lambda from theta and Lind
-	double *Lamx = (double*)d_Lambda.x;//, *th = nt.begin();
+	double *Lamx = (double*)d_Lambda.x, *th = nt.begin();
 	int *Li = d_Lind.begin();
 	for (R_len_t i = 0; i < d_Lind.size(); i++) Lamx[i] = th[Li[i] - 1];
     }
