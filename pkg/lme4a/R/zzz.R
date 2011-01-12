a5e903998e97934f33c62eb641f8c6c0c7f53044
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2011-01-12 21:45:07 +0000

    restore populate call
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@1186 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/lme4a/R/zzz.R
+++ pkg/lme4a/R/zzz.R
@@ -2,8 +2,6 @@
 
 .onLoad <- function(libname, pkgname)
 {
-    ## For Matrix API change (Oct.2009) - silence the warning:
-    assign("det_CHMfactor.warn", FALSE, envir = Matrix:::.MatrixEnv)
-    mod <- Module("lme4")
-    populate(mod, .NameSpace)
+     mod <- Module("lme4")
+     populate(mod, .NameSpace)
 }
