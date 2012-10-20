b827506279d1e70e89daa332e96c5834c69520dc
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-20 16:27:49 +0000

    add integrateGQ() and some interesting examples
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@1790 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Gqr/R/GaussQuad.R
+++ pkg/Gqr/R/GaussQuad.R
@@ -1,15 +1,32 @@
 GaussQuad <-
-    function(n, rule, a=0, b=1, alpha=0, beta=0) {
-        rules <- c("Legendre", "Chebyshev", "Gegenbauer", "Jacobi",
-                   "Laguerre", "Hermite", "Exponential", "Rational", "Type2")
-        if (is.na(rr <- pmatch(rule, rules)))
-            stop("Unknown quadrature rule, ", rule)
-        stopifnot(length(n <- as.integer(n)) == 1L,
-                  length(a <- as.numeric(a)) == 1L,
-                  length(b <- as.numeric(b)) == 1L,
-                  length(alpha <- as.numeric(alpha)) == 1L,
-                  length(beta <- as.numeric(beta)) == 1L,
-                  n > 0L)
-        .Call(Gauss_Quad, n, rr, a, b, alpha, beta)
-    }
+    function(n, rule = c("Legendre", "Chebyshev", "Gegenbauer", "Jacobi",
+                         "Laguerre", "Hermite", "Exponential", "Rational"),
+             a=0, b=1, alpha=0, beta=0)
+{
+    ## Note: rule "Type2" is not yet supported in C code
+    rules <- eval(formals()$rule)
+    if (is.na(rr <- pmatch(rule, rules)))
+        stop("Unknown quadrature rule, ", rule)
+    stopifnot(length(n <- as.integer(n)) == 1L,
+              length(a <- as.numeric(a)) == 1L,
+              length(b <- as.numeric(b)) == 1L,
+              length(alpha <- as.numeric(alpha)) == 1L,
+              length(beta <- as.numeric(beta)) == 1L,
+              n > 0L)
+    .Call(Gauss_Quad, n, rr, a, b, alpha, beta)
+}
 
+integrateGQ <- function(f, lower, upper, ...,
+                        n = 13, rule = "Legendre",
+                        a=lower, b=upper, alpha=0, beta=0)
+{
+    f <- match.fun(f)
+    stopifnot(length(a) == 1, is.finite(a),
+              length(b) == 1, is.finite(b),
+              length(alpha) == 1, is.finite(alpha),
+              length(beta) == 1, is.finite(beta))
+    rules <- eval(formals(GaussQuad)$rule)
+    rule <- match.arg(rule, choices = rules)
+    GQ <- GaussQuad(n, rule=rule, a=a, b=b, alpha=alpha, beta=beta)
+    with(GQ, sum(weights * f(knots, ...)))
+}
--- pkg/Gqr/tests/GH_test.R
+++ pkg/Gqr/tests/GH_test.R
@@ -1,10 +1,24 @@
 library(Gqr)
-wfmt <- "http://people.sc.fsu.edu/~jburkardt/datasets/quadrature_rules_hermite_probabilist/hermite_probabilist_%03d_w.txt "
-xfmt <- "http://people.sc.fsu.edu/~jburkardt/datasets/quadrature_rules_hermite_probabilist/hermite_probabilist_%03d_x.txt "
+
+## Do not do the checks per default "on CRAN"
+(doExtras <- {
+    interactive() ||
+        grepl("bates", Sys.getenv("USER")) ||
+        nzchar(Sys.getenv("R_Gqr_check_extra")) ||
+        identical("true", unname(Sys.getenv("R_PKG_CHECKING_doExtras")))
+})
+
+if(doExtras) {
+    urldir <- "http://people.sc.fsu.edu/~jburkardt/datasets/quadrature_rules_hermite_probabilist/"
+    wfmt <- paste0(urldir,"hermite_probabilist_%03d_w.txt")
+    xfmt <- paste0(urldir,"hermite_probabilist_%03d_x.txt")
+}
 nn <- 2^(1:7) - 1L
 for (n in nn) {
     loc <- GaussQuad(n, "H", b=0.5)
-    webw <- scan(url(sprintf(wfmt, n)))
-    webx <- scan(url(sprintf(xfmt, n)))
-    stopifnot(all.equal(webw, loc$weights), all.equal(webx, loc$knots))
+    if(doExtras) {
+        webw <- scan(url(sprintf(wfmt, n)))
+        webx <- scan(url(sprintf(xfmt, n)))
+        stopifnot(all.equal(webw, loc$weights), all.equal(webx, loc$knots))
+    }
 }
