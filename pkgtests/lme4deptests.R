5b7168c9a22add20264a70e7f8159282e71866bd
bbolker (Ben  Bolker) <bolker@mcmaster.ca>
2013-04-17 21:09:55 +0000

    update pkgtests machinery
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@1810 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkgtests/lme4deptests.R
+++ pkgtests/lme4deptests.R
@@ -2,43 +2,43 @@
 ##
 ## include all downstream packages from CRAN, r-forge:
 ## packages to check, loaded from package-dependency scan
-X <- read.csv("lme4_depends.csv",as.is=TRUE,fileEncoding="UTF8")
-source("lme4depfuns.R")
-skippkgs <- "polytomous"
 
-## * must export R_LIBS_SITE=./library before running R CMD BATCH?  (why?)
-## * make sure that .R/check.Renviron is set, e.g.
-##   mv /home/bolker/.R/check.Renviron.old  ~/.R/check.Renviron
-##     ~/.R/check.Renviron:
-##        R_LIBS_SITE=/home/bolker/R/pkgs/lme4/testpkgs/library
+source("http://developer.r-project.org/CRAN/Scripts/depends.R")
+rr <- reverse_dependencies_with_maintainers("lme4")
+source("lme4depfuns.R")  ## component/utility functions
+## packages to skip (because dependencies don't install etc.
+## skippkgs <- "polytomous"
+skippkgs <- character(0)
+
+## * must export R_LIBS_SITE=./library before running R CMD BATCH
+##   and  make sure that .R/check.Renviron is set
+##   (this is done in the 'runtests' script)
 
 ##  (repeated from lme4depfuns.R):
-##    FIXME: check for/document local version of tarball more recent than R-forge/CRAN versions
-##    FIXME: consistent implementation of checkdir
+##   FIXME: check for/document local version of tarball more recent than R-forge/CRAN versions
+##  currently just tries to find most recent version and check it, but could check all versions?
+##  FIXME: consistent implementation of checkdir
 
 ## FIXME: set up an appropriate makefile structure for this ? (a little tricky if it also depends on
 ##   checking CRAN/R-forge versions?
-
-## FIXME (?)/warning: need to make sure that latest version of lme4 is installed in libdir ...
+##  might to be able to use update.packages() ...
+  
+## FIXME (?)/warning: need to make sure that latest/appropriate version of lme4 is installed locally ...
 
 ## FIXME: why are R2admb, RLRsim, sdtalt, Zelig not getting checked?
 
-testdir <- "~/R/pkgs/lme4/pkgtests"
+testdir <- getwd()
+
+## directory for tarballs to check
 tarballdir <- file.path(testdir,"tarballs")
+## directory for ?? (at least current lme4 version ...)
 libdir <-     file.path(testdir,"library")
 
 suppressWarnings(rm(list=c("availCRAN","availRforge"))) ## clean up
-options(repos="http://probability.ca/cran")
 
-require(tools)
-## assume wd is test dir, already
-if (FALSE) {
-    setwd(testdir)
-}
+## require(tools)
 
-## auto-generated list -- cleaned up by hand (removed 'lme4', fixed R2STATS maintainer syntax,
-##   fixed comma in LMERconveniencefunctions author)
-
+## make directories ...
 dir.create(tarballdir,showWarnings=FALSE)
 dir.create(libdir,showWarnings=FALSE)
 ## want to install additional dependencies etc. out of the way
@@ -46,7 +46,7 @@ dir.create(libdir,showWarnings=FALSE)
 ## it would be nice to use tools:::testInstalledPackages(), but I may simply
 ##  have to do R CMD check
 
-pkgnames <- X$X
+pkgnames <- rr[,"Package"]
 names(pkgnames) <- pkgnames ## so results are named
 pkgnames <- pkgnames[!pkgnames %in% skippkgs]
 require(parallel)
