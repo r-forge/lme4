8804809d90328de7babef290c67998fe0cbc86d0
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2013-06-06 22:16:44 +0000

    More explanation on the general update and sample code.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@1822 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/JSS/lmer.jl
+++ www/JSS/lmer.jl
@@ -1,7 +1,10 @@
 using DataFrames
-using Base.LinAlg.BLAS: trmm, gemv!
+using Base.LinAlg.BLAS: trmm, gemm!
+using Base.LinAlg.CHOLMOD: CholmodFactor, chm_factorize_p!
 
 type MixedModel{Ti<:Union(Int32,Int64)}
+    L::CholmodFactor{Float64,Ti}
+    LambdatZt::CholmodSparse{Float64,Ti}
     X::ModelMatrix                      # (dense) model matrix
     Xs::Vector{Matrix{Float64}}         # X_1,X_2,...,X_k
     beta::Vector{Float64}
@@ -11,6 +14,27 @@ type MixedModel{Ti<:Union(Int32,Int64)}
     y::Vector{Float64}
 end
 
+function updateL(m::MixedModel, theta::Vector{Float64})
+    n,p = size(m.X)
+    lambda = m.lambda
+    pvec = map((x)->size(x,1), m.lambda)
+    tlen = mapreduce((p)->(p*(p+1))>>1, +, pvec)
+    nzmat = reshape(m.LambdatZt.nzvals, (sum(pvec),n))
+    tpos = 1; roff = 0                    # position in theta, row offset
+    for i in 1:length(pvec)
+        T = lambda[i]
+        p_i = size(T,1)
+        for j in 1:p_i, i in j:p_i
+            T[i,j] = theta[tpos]; tpos += 1
+            if i == j && T[i,j] < 0. error("Negative diagonal element in T") end
+        end
+        gemm!('T','T',1.0,T,Xs[i],0.0,sub(nzmat,roff+(1:p_i),1:n))
+        roff += p_i
+    end
+    chm_factorize_p!(m.L,m.LambdaZt,1.)
+    m
+end
+        
 function linpred(m::MixedModel)
     lp = m.X.m * m.beta                 # initialize value to X*beta
     lm = m.lambda
