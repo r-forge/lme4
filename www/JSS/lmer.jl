a146924900a3129d80692a1c6513538b2f9cd57b
dmbates (Professor Emeritus ) <dmbates@gmail.com>
2013-06-07 21:48:38 +0000

    Tighten up the updateL function a bit.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@1823 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/JSS/lmer.jl
+++ www/JSS/lmer.jl
@@ -5,7 +5,7 @@ using Base.LinAlg.CHOLMOD: CholmodFactor, chm_factorize_p!
 type MixedModel{Ti<:Union(Int32,Int64)}
     L::CholmodFactor{Float64,Ti}
     LambdatZt::CholmodSparse{Float64,Ti}
-    X::ModelMatrix                      # (dense) model matrix
+    X::ModelMatrix                      # fixed-effects model matrix
     Xs::Vector{Matrix{Float64}}         # X_1,X_2,...,X_k
     beta::Vector{Float64}
     inds::Matrix{Ti}                    # n by k
@@ -15,21 +15,20 @@ type MixedModel{Ti<:Union(Int32,Int64)}
 end
 
 function updateL(m::MixedModel, theta::Vector{Float64})
-    n,p = size(m.X)
-    lambda = m.lambda
-    pvec = map((x)->size(x,1), m.lambda)
-    tlen = mapreduce((p)->(p*(p+1))>>1, +, pvec)
-    nzmat = reshape(m.LambdatZt.nzvals, (sum(pvec),n))
-    tpos = 1; roff = 0                    # position in theta, row offset
-    for i in 1:length(pvec)
+    n,k = size(m.inds)
+    LamZ = m.LambdatZt
+    N = reshape(LamZ.nzvals, (div(length(LamZ),n),n))
+    lambda = m.lambda; Xs = m.Xs
+    tpos = 1; roff = 0                  # position in theta, row offset
+    for i in 1:k
         T = lambda[i]
-        p_i = size(T,1)
-        for j in 1:p_i, i in j:p_i
+        p = size(T,1)                   # size of i'th template matrix
+        for j in 1:p, i in j:p          # fill lower triangle from theta
             T[i,j] = theta[tpos]; tpos += 1
             if i == j && T[i,j] < 0. error("Negative diagonal element in T") end
         end
-        gemm!('T','T',1.0,T,Xs[i],0.0,sub(nzmat,roff+(1:p_i),1:n))
-        roff += p_i
+        gemm!('T','T',1.0,T,Xs[i],0.0,sub(nzmat,roff+(1:p),1:n))
+        roff += p
     end
     chm_factorize_p!(m.L,m.LambdaZt,1.)
     m
@@ -47,3 +46,4 @@ function linpred(m::MixedModel)
     end
     lp
 end
+
