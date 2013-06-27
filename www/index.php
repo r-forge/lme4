c44b6c86f62b3c2a979a8526dd51f40816aec6d9
bbolker (Ben  Bolker) <bolker@mcmaster.ca>
2013-06-27 04:56:20 +0000

    update front page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@1826 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,74 +43,17 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<h2>Resources</h2>
+<code>lme4</code> development has moved to <a href="https://github.com/lme4/lme4/">Github</a>; please go there for up-to-date development versions and information.  
+
+<h3>Resources that remain on R-forge</h3>
 <ul>
-<li>A <a href="misc/lme4_conversion.html">guide</a> to changes in the upcoming release of <code>lme4</code> (<a href="misc/lme4_conversion.md">Markdown source</a>, <a href="misc/lme4_compat_report.html">downstream package compatibility report</a>)</li>
-<li>The <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/">project summary page</a>, including links to the 
-<a href="http://r-forge.r-project.org/scm/?group_id=60">source code browser</a>, 
-<a href="http://r-forge.r-project.org/tracker/?group_id=60">bug and feature request trackers</a>, 
-<a href="http://r-forge.r-project.org/R/?group_id=60">compiled package files</a>, etc.</li>
-<li><a href="./doxygen/"><em>Doxygen</em> documentation</a> of the underlying C functions</li>
 <li><a href="bib/lme4bib.html">References to articles and other research</a> using <code>nlme</code> or <code>lme4</code>, or the corresponding
 <a href="bib/lme4bib.bib">BibTeX file</a>. (If you would like to add your work to this database, please email <code>vasishth.shravan at gmail dot com</code>.)</li>
 <li><a href="./slides/">Slides</a> from short courses on <code>lme4</code></li>
 <li><a href="./lMMwR/">Chapter drafts</a> of the book <em>lme4:
 Mixed-effects Modeling with R</em></li>
-
-<h2>Installation</h2>
-<p>Binary and source packages of recent versions
-of the development version  (<code>lme4</code>, previously <code>lme4Eigen</code>) are available
-<a href="./repos"><strong>here</strong></a>,
-as well as MacOS binaries for <code>RcppEigen</code>.
-The current status for the various versions is as follows:
-<ul>
-<li>The stable release of <code>lme4</code> on CRAN (version 0.999375-42) is the last version of this branch.</li>
-<li>R-forge has <code>lme4.0</code> (version 0.9999-1), which is the continuation of the old branch.  It should be perfectly compatible with the current CRAN version, <emph>except</emph> for its package name.</li>
-<li>R-forge also has a new version of <code>lme4</code> (version 0.999902344-0), which is as far as possible <emph>externally</emph> compatible with the old branch, but its internal structures have changed, so packages dependent on the old structures will need to be updated to work with the new release.  The new version also produces objects of class <code>merMod</code> rather than class <code>mer</code>.</li>
-<li>An older development branch, <code>lme4a</code>, is also available on R-forge, but we believe that it is now dominated by the new development branch (i.e. the r-forge version of <code>lme4</code>), and we strongly request that you work with the new version. If you encounter a situation where <code>lme4</code> can't do something that <code>lme4a</code> can, please inform the maintainers.
-</li>
+<p><a href="./repos">Repository</a> containing (relatively) up-to-date binary and source packages of recent versions
+of the development version (for most up-to-date code, which must be installed from source, see Github).
 </ul>
-<p>
-<ul>
-<li>Although installing packages into older versions of R is sometimes possible by downloading and manually installing the package files, installation will generally work <emph>much</emph> more smoothly if you are running the latest version of R.  If you want help installing <code>lme4</code> to an older version of R you will have to provide a very good reason ...
-</li>
-<li>You may have to install dependencies 
-(e.g. <code>RcppEigen</code> and <code>minqa</code>
-for <code>lme4</code>) from other repositories first.</li>
-</ul>
-<ol>
-<li>If possible, you should install the most recent version of the development version built on r-forge, e.g.
-<pre>
-install.packages("lme4",repos="http://r-forge.r-project.org")
-</pre>.
-</li>
-<li>If the appropriate version for your OS is not available 
-(e.g. binaries for MacOS), the next best option is to install
-the most recent version available at the <code>lme4</code>
-repository, e.g.
-<pre>
-install.packages("lme4",repos="http://lme4.r-forge.r-project.org/repos")
-</pre>
-These versions will generally be slightly less up-to-date than the ones built by R-forge, because they must be updated by hand.
-</li>
-<li>
-Alternatively (e.g. if you have network problems),
-you should be able to install these versions
-by descending through the directory hierarchy 
-from <a href="http://lme4.r-forge.r-project.org/repos">here</a>,
-downloading a copy and using
-<pre>
-install.packages("[name_of_file_here]",repos=NULL)
-</pre>
-or a menu item such as "install from local zip file"
-</li>
-<li>The most reliable way to install the package 
-is to check out 
-<a href="http://r-forge.r-project.org/scm/?group_id=60">the most recent copy</a> and build it from source on your machine; however, this may require extra tools (Subversion client, compilers, etc.).  Alternatively, if you have the compiler tools but don't want to mess with Subversion, you will often get farther with <code>install.packages(...,type="source")</code> than trying to find a binary package compiled for your OS.
-</li>
-<li>
-If all else fails, please contact the maintainers.
-</li>
-</p>
 </body>
 </html>
