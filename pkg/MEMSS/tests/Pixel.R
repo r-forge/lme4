e2915bcf122bb59f44a5dbb1725479f387ce8344
walker (Steve Walker) <steve.walker@utoronto.ca>
2014-03-11 14:05:29 +0000

    prepare for maintenance cran submission
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lme4@1884 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/MEMSS/tests/Pixel.R
+++ pkg/MEMSS/tests/Pixel.R
@@ -1,7 +1,7 @@
 library(MEMSS)
-options(show.signif.stars = FALSE)
+options(show.signif.stars = FALSE, digits = 3)
 m1 <- lmer(pixel ~ day + I(day^2) + (1|Dog:Side) + (day|Dog),
-           Pixel, verbose = TRUE)
+           Pixel)
 print(m1, corr = FALSE)
 ranef(m1)
 q("no")
